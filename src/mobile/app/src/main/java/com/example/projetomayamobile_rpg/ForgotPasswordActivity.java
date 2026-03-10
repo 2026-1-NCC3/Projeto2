547c1856c7e73a3935cdc565fddc4cb3284c43a6
Said Sales <cmcsaas@gmail.com>
2026-03-10 13:41:38 -0300

    Finalização da tela de login e tela de recuperação de senha finalizada

--- src/mobile/app/src/main/java/com/example/projetomayamobile_rpg/ForgotPasswordActivity.java
+++ src/mobile/app/src/main/java/com/example/projetomayamobile_rpg/ForgotPasswordActivity.java
@@ -3,11 +3,11 @@ package com.example.projetomayamobile_rpg;
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
+public class ForgotPasswordActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.forgotpassword_activity);
     }
 }
