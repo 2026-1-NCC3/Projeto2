922106a6292c0de43331d2bbe5e7b4c126c1c41d
GianLemes <gianlemes7@gmail.com>
2026-03-15 00:31:34 -0300

    tela do Dashboard sem os detalhes finais

---
+++ src/mobile/app/src/main/java/com/example/projetomayamobile_rpg/DashboardActivity.java
@@ -0,0 +1,13 @@
+package com.example.projetomayamobile_rpg;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class DashboardActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.dashboard_activity);
+    }
+}
