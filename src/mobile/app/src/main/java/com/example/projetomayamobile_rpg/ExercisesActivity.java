cbbe5f75fe22264659054ec3d42637d9ff81e866
Said Sales <cmcsaas@gmail.com>
2026-03-17 15:47:48 -0300

    iniciando a construção do plano de exercícios

---
+++ src/mobile/app/src/main/java/com/example/projetomayamobile_rpg/ExercisesActivity.java
@@ -0,0 +1,13 @@
+package com.example.projetomayamobile_rpg;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ExercisesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.exercises_activity);
+    }
+}
