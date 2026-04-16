06bf6b6bca889d6fae3c99147e58ac1c589079bd
Gabriel Diniz <gabrielvmdiniz12@gmail.com>
2026-04-16 10:57:40 -0300

    FIx: plans repository

--- src/backend/projeto-maya-rpg/src/main/java/com/beholders/projeto_maya_rpg/repository/PlansRepository.java
+++ src/backend/projeto-maya-rpg/src/main/java/com/beholders/projeto_maya_rpg/repository/PlansRepository.java
@@ -4,8 +4,5 @@ import com.beholders.projeto_maya_rpg.model.Plan;
 import org.hibernate.query.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.awt.print.Pageable;
-
 public interface PlansRepository extends JpaRepository<Plan, Long> {
-    Page findAll(Pageable pageable);
 }
