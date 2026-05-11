95ff51d8da4f0f7680f7cee29fe00ed89b200bf6
leticia19silva <leticia19contato@gmail.com>
2026-05-11 01:06:12 -0300

    fix: urls corrigidas - estava rodando localmente

--- src/mobile/app/src/main/java/com/example/projetomayamobile_rpg/activitys_javaclass/ForgotPasswordActivity.java
+++ src/mobile/app/src/main/java/com/example/projetomayamobile_rpg/activitys_javaclass/ForgotPasswordActivity.java
@@ -25,7 +25,7 @@ import retrofit2.converter.scalars.ScalarsConverterFactory;
 public class ForgotPasswordActivity extends AppCompatActivity {
 
     private static final String EMAIL_SUBJECT = "Recuperação de senha";
-    private static final String BASE_URL = "http://192.168.0.22:8080/";
+    private static final String BASE_URL = "http://54.233.94.214:8080/";
 
     MaterialButton btnSendCode;
     MaterialButton btnBackLogin;
